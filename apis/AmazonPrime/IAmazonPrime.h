f6712dcef77a46828a5d01486e3036bb208dbba0
ssitar583 <soundaryaa_sitaram@comcast.com>
2025-07-23 12:16:47 +0530

    Update IAmazonPrime.h

--- apis/AmazonPrime/IAmazonPrime.h
+++ apis/AmazonPrime/IAmazonPrime.h
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-//Test1
+//Test2
 #ifndef __IAMAZON_PRIME_H
 #define __IAMAZON_PRIME_H
 
