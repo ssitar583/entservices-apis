f2edad00fb9ee42950f08023c757a10072ea43df
andrejz2 <113075729+andrejz2@users.noreply.github.com>
2025-07-28 09:48:48 -0400

    Update generate_md_from_header.py
    Update supports doc gen if a plugin has multiple .h in the folder

--- tools/md_from_h_generator/generate_md_from_header.py
+++ tools/md_from_h_generator/generate_md_from_header.py
@@ -23,53 +23,77 @@ import os
 import argparse
 from header_file_parser import HeaderFileParser
 from logger import Logger
-from markdown_templates import generate_header_description_markdown, generate_header_toc, generate_methods_toc, generate_method_markdown, generate_notifications_toc, generate_notification_markdown, generate_properties_toc, generate_property_markdown
+from markdown_templates import generate_header_description_markdown, generate_header_toc, generate_methods_toc, generate_method_markdown, generate_notifications_toc, generate_notification_markdown, generate_properties_toc, generate_property_markdown, generate_configuration_options_section
 
-def generate_md_from_header(header_file):
+def combine_header_structures(main_structure, additional_structure):
+    main_structure.methods.update(additional_structure.methods)
+    main_structure.properties.update(additional_structure.properties)
+    main_structure.events.update(additional_structure.events)
+    main_structure.symbols_registry.update(additional_structure.symbols_registry)
+    main_structure.iterators_registry.update(additional_structure.iterators_registry)
+
+def generate_md_from_header(plugin_folder_path, individual=False):
     """
     Writes the markdown documentation from the header file.
 
     Args:
         header_file (str): Path to the header file.
     """
-    filename = os.path.basename(header_file)
-    classname, _ = os.path.splitext(filename)
+    header_files = [f for f in os.listdir(plugin_folder_path) if f.endswith('.h')]
+    plugin_name = os.path.basename(plugin_folder_path)
 
-    log_file_path = './tools/md_from_h_generator/logs/' + classname + '.txt'
-    os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
-    logger = Logger(log_file_path)
+    # if individual flag, then we generate a separate markdown file for each header file in folder
+    if individual:
+        for header_file in header_files:
+            header_file_path = os.path.join(plugin_folder_path, header_file)
+            classname = os.path.splitext(header_file)[0][1:]
+            log_file_path = f'./tools/md_from_h_generator/logs/{classname}.txt'
+            logger = Logger(log_file_path)
+            header_structure = HeaderFileParser(header_file_path, classname, logger)
+            generate_md_from_individual_header_file(header_structure, classname, logger)
+    else:
+        classname = plugin_name
+        log_file_path = f'./tools/md_from_h_generator/logs/{classname}.txt'
+        logger = Logger(log_file_path)
+        main_header_file_path = os.path.join(plugin_folder_path, header_files[0])
+        main_structure = HeaderFileParser(main_header_file_path, classname, logger)
+        # if folder contains more than one header files, combine their structures
+        for header_file in header_files[1:]:
+            header_file_path = os.path.join(plugin_folder_path, header_file)
+            additional_structure = HeaderFileParser(header_file_path, classname, logger)
+            combine_header_structures(main_structure, additional_structure)
+        generate_md_from_individual_header_file(main_structure, classname, logger)
 
-    header_structure = HeaderFileParser(header_file, logger)
-    header_structure.build_all_canonical_dicts()
-
-    # Remove the leading 'I' from the api's class name
-    output_file_path = './tools/md_from_h_generator/generated_docs/' + header_structure.classname + 'Plugin.md'
+def generate_md_from_individual_header_file(header_structure, classname, logger):
+    output_file_path = './tools/md_from_h_generator/generated_docs/' + classname + 'Plugin.md'
     os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
     with open(output_file_path, 'w', encoding='utf-8') as file:
-        file.write(generate_header_toc(header_structure.classname, header_structure))
-        file.write(generate_header_description_markdown(header_structure.classname, getattr(header_structure, 'plugindescription', '')))
+        file.write(generate_header_toc(classname, header_structure))
+        file.write(generate_header_description_markdown(classname, getattr(header_structure, 'plugindescription', '')))
+        file.write(generate_configuration_options_section(header_structure.configuration_options))
         if len(header_structure.methods.values()) > 0:
-            file.write(generate_methods_toc(header_structure.methods, header_structure.classname))
+            file.write(generate_methods_toc(header_structure.methods, classname))
             for method_name, method_info in header_structure.methods.items():
                 file.write(generate_method_markdown(
-                    method_name, method_info, header_structure.symbols_registry, header_structure.classname, header_structure.events))
+                    method_name, method_info, header_structure.symbols_registry, classname, header_structure.events))
         file.write("\n")
         if len(header_structure.properties.values()) > 0:
-            file.write(generate_properties_toc(header_structure.properties, header_structure.classname))
+            file.write(generate_properties_toc(header_structure.properties, classname))
             for prop_name, prop_info in header_structure.properties.items():
                 file.write(generate_property_markdown(
-                    prop_name, prop_info, header_structure.symbols_registry, header_structure.classname))
+                    prop_name, prop_info, header_structure.symbols_registry, classname))
         file.write("\n")
         if len(header_structure.events.values()) > 0:
-            file.write(generate_notifications_toc(header_structure.events, header_structure.classname))
+            file.write(generate_notifications_toc(header_structure.events, classname))
             for event_name, event_info in header_structure.events.items():
                 file.write(generate_notification_markdown(
-                    event_name, event_info, header_structure.symbols_registry, header_structure.classname))
+                    event_name, event_info, header_structure.symbols_registry, classname))
     logger.write_log()
     logger.close()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process header file.')
-    parser.add_argument('header_file', type=str, help='Path to header file')
+    parser.add_argument('plugin_folder', type=str, help='Path to plugin folder')
+    parser.add_argument('--individual', action='store_true', help='Specify if output doc should be generated for all headers in folder or if they should be generated individually')
     args = parser.parse_args()
-    generate_md_from_header(args.header_file)
+    generate_md_from_header(args.plugin_folder, args.individual)
